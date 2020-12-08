c3a1dc8df5e0a77e30244ac26320f53c460a8715
Ivan San Martin Farias <ivan.sanmartin98@gmail.com>
2020-12-08 20:13:47 -0300

    Se modifica la vista de la ventana de home

--- src/Home.jsx
+++ src/Home.jsx
@@ -19,22 +19,21 @@ const Home = () => {
     
     return (
       <div className="container" style={{padding: '25px 10px'}}>
-      <div className="row">
-        <div className=" col-md-4 main">
-            <h1 className="component-center">Piedra Papel Tijera</h1>
-            <h1 className="component-center">Lagarto Spock</h1>
-          <div className="component-center">
-            <input onChange={handleNameChange} placeholder="Ingrese nombre jugador" className="form-control"/> 
-            <h2 className="component-center">VS Bot</h2>
-            <button onClick={handleClick} className="btn btn-outline-danger">
-                Iniciar Partida
-            </button>
+        <div className="row">
+          <div className=" col-md-4 main">
+              <h1 className="component-center">Piedra Papel Tijera</h1>
+              <h1 className="component-center">Lagarto Spock</h1>
+            <div className="component-center">
+              <input onChange={handleNameChange} placeholder="Ingrese nombre jugador" className="form-control margin-top"/> 
+              <button onClick={handleClick} className="btn btn-outline-danger margin">
+                  Iniciar Partida
+              </button>
+            </div>
+          </div>
+          <div className="col-md-4 margin-top">
+            <img src="/the-big-bang-theory.png" style={{width: '45rem'}}/>
           </div>
         </div>
-        <div className="col-md-4 margin-top">
-          <img src="/the-big-bang-theory.png" style={{width: '45rem'}}/>
-        </div>
-      </div>
       </div>
     );
 }
